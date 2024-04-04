f01ad0526b6cc1ea5eb98626e0e9c45974d5c8e7
kevinnadr <kevin19305.ib@gmail.com>
2024-04-04 19:10:00 +0700

    void display revisi

--- AlgoritmaSearch.cpp
+++ AlgoritmaSearch.cpp
@@ -64,13 +64,7 @@ void LinearSearch()
 void display ()
 {
     cout << "Array element are: ";
-    for (i = 0; i < n; 1++)
+    for (i = 0; i < n; i++)
         cout << arr[i] << " ";
 }
 
-int main()
-{
-    input();
-    LinearSearch();
-    display();
-}
